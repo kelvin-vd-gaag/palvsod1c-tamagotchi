6fbaf1e50f6d757b29606a61b2b67f84bbe58196
Kelvin van der Gaag <kelvinvandergaag@protonmail.com>
2026-01-16 14:36:53 +0100

    Opdracht 1 en 2 af

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 let hunger = 50;
-
+let energy = 50;
+let joy = 50;
 document.getElementById("hunger-value").innerText = "Honger: " +  hunger;
+document.getElementById("energy-value").innerText = "Energie: " +  energy;
+document.getElementById("joy-value").innerText = "Plezier: " +  joy;
+
+let music = new Audio("background-music.mp3");
+
+
+function start_game(){
+    music.play();
+    document.getElementById("start-game-menu").style.display = "none";
+}
